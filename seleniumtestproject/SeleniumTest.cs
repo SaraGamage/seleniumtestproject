f1cdf185f9646fd0ced9b44c790dfe851b76108b
Sara Gamage <saragamage@gmail.com>
2024-07-14 19:15:49 +1000

    refactor

--- seleniumtestproject/SeleniumTest.cs
+++ seleniumtestproject/SeleniumTest.cs
@@ -13,22 +13,13 @@ namespace SeleniumTestProject
     {
         private IWebDriver driver;
         public IDictionary<string, object> vars { get; private set; }
-        private IJavaScriptExecutor js;
-
-        private string TestingEnvironment = "Development?";
-        private string TestingVersionOrCommit = "Unknown";
-        private bool IsProduction = false;
+        private IJavaScriptExecutor? js;
+        private bool isProduction = false;
         
-        [OneTimeSetUp]
+         [OneTimeSetUp]
         public void RunBeforeAnyTests()
         {
-            TestingEnvironment = Environment.GetEnvironmentVariable("TEST_ENV");
-            TestingVersionOrCommit = Environment.GetEnvironmentVariable("TEST_VER");
-            Console.WriteLine($"-----------------------------------------------");
-            Console.WriteLine($"TestingEnvironment {TestingEnvironment}");
-            Console.WriteLine($"TestingVersionOrCommit {TestingVersionOrCommit}");
-            Console.WriteLine($"-----------------------------------------------");
-            IsProduction = TestingEnvironment.Equals("Production", StringComparison.CurrentCultureIgnoreCase);
+            TestManagement.CheckEnvironment(isProduction);
         }
 
         [SetUp]
@@ -36,70 +27,33 @@ namespace SeleniumTestProject
         {
             var chromeOptions = new ChromeOptions();
             chromeOptions.AddArguments("--headless");
-            chromeOptions.AddArguments("--verbose"); 
+            chromeOptions.AddArguments("--verbose");
             //chromeOptions.AddArguments("--output-directory=results");
             //new diver created
             driver = new ChromeDriver(chromeOptions);
-            
+            //driver = new ChromeDriver();
             js = (IJavaScriptExecutor)driver;
             vars = new Dictionary<string, object>();
         }
 
         [TearDown]
-        protected void TearDown() => driver?.Quit();
-
-        public string waitForWindow(int timeout)
+        protected void TearDown()
         {
-            try
-            {
-                Thread.Sleep(timeout);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("{0} Exception caught.", e);
-            }
-            var whNow = ((IReadOnlyCollection<object>)driver.WindowHandles).ToList();
-            var whThen = ((IReadOnlyCollection<object>)vars["WindowHandles"]).ToList();
-            if (whNow.Count > whThen.Count)
-            {
-                return whNow.Except(whThen).FirstOrDefault().ToString();
-            }
-            else
-            {
-                return whNow.First().ToString();
-            }
+            driver.Quit();
+            driver.Dispose();
         }
-
-        [Test, Description("Can load page")]
-        public void Test01()
-        {
-            //this is a test
-            driver.Navigate().GoToUrl("https://www.selenium.dev/selenium/web/web-form.html");
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(2000);
-            TestCases.CanRunTestHittingWebsiteOfMyChoosing(driver);
-            Assert.Pass();
-        }
-
+      
         [Test, Description("Submit google search")]
-        public void Test02()
+        public void GoogleSearch()
         {
-            // 2. Navigate to the URL
-            driver.Navigate().GoToUrl("https://www.google.com/");
-            // 2a. Maximize the browser window
-            driver.Manage().Window.Maximize();
-            // 3. Find the element
-            IWebElement webElement = driver.FindElement(By.Name("q"));
-            // 4. Type in the element
-            webElement.SendKeys("Selenium");
-            // 5. Click on the element
-            
-            webElement.SendKeys(Keys.Return);
-            var url = driver.FindElement(By.ClassName("byrV5b"));
-            
-            url.Click();
+            TestManagement.TestHittingGoogleSearch(driver);
 
-            //webElement.Click();
+        }
 
+        [Test, Description("Can load page of my choice")]
+        public void TestHittingSomeWebsite()
+        {
+            TestManagement.TestHittingSomeWebsite(driver);
         }
 
         [Test, Description("Always passes")]
@@ -123,7 +77,7 @@ namespace SeleniumTestProject
         [Test, Description("A very specific test that needs to do 1 thing in dev, another in prod")]
         public void TwoSecondTestOnDevHalfSecondOnProd()
         {
-            if(IsProduction)
+            if (isProduction)
             {
                 Console.WriteLine($"If Prod then we're going to not sleep as long");
                 Thread.Sleep(500);
@@ -140,7 +94,7 @@ namespace SeleniumTestProject
         {
             Assert.That(true);
         }
-        
+
         [Test, Description("Always passes")]
         public void Test08()
         {
---
+++ seleniumtestproject/TestManagement..cs
@@ -0,0 +1,77 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace SeleniumTestProject
+{
+    public static class TestManagement
+    {
+        public static void SetUpDriver(IWebDriver? driver, ChromeOptions chromeOptions, IJavaScriptExecutor? js)
+        {
+            chromeOptions.AddArguments("--headless");
+            chromeOptions.AddArguments("--verbose");
+            //chromeOptions.AddArguments("--output-directory=results");
+            //new diver created
+            driver = new ChromeDriver(chromeOptions);
+            //driver = new ChromeDriver();
+            js = (IJavaScriptExecutor)driver;
+            
+        }
+
+        public static void CheckEnvironment( bool isProduction)
+        {
+            isProduction = false;
+            string testingEnvironment = Environment.GetEnvironmentVariable("TEST_ENV") ?? "Development";
+            string testingVersionOrCommit = Environment.GetEnvironmentVariable("TEST_VER") ?? "Unknown";
+            Console.WriteLine($"-----------------------------------------------");
+            Console.WriteLine($"TestingEnvironment {testingEnvironment}");
+            Console.WriteLine($"TestingVersionOrCommit {testingVersionOrCommit}");
+            Console.WriteLine($"-----------------------------------------------");
+           
+            isProduction = testingEnvironment.Equals("Production", StringComparison.CurrentCultureIgnoreCase);
+           
+        }
+
+        public static void TestHittingSomeWebsite(IWebDriver driver)
+        {
+            //this is a test
+            driver.Navigate().GoToUrl("https://www.selenium.dev/selenium/web/web-form.html");
+            var title = driver.Title;
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(2000);
+
+            var textBox = driver.FindElement(By.Name("my-text"));
+            var submitButton = driver.FindElement(By.TagName("button"));
+
+            textBox.SendKeys("Selenium");
+            submitButton.Click();
+
+            var message = driver.FindElement(By.Id("message"));
+            var actual = message.Text;
+
+            Assert.That(actual, Is.EqualTo("Received!"));
+
+        }
+
+        public static void TestHittingGoogleSearch(IWebDriver driver)
+        {
+            // 2. Navigate to the URL
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            // 2a. Maximize the browser window
+            driver.Manage().Window.Maximize();
+            // 3. Find the element
+            IWebElement webElement = driver.FindElement(By.Name("q"));
+            // 4. Type in the element
+            webElement.SendKeys("Selenium");
+            // 5. Click on the element
+            webElement.SendKeys(Keys.Return);
+            var url = driver.FindElement(By.PartialLinkText("Selenium"));
+            if (url != null)
+            {
+                url.Click();
+                Assert.That(true);
+            }
+            else 
+            Assert.That(false);
+        }
+    }
+}
